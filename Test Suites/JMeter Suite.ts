bd5de676f16a512488e06bf4405b2b3614e45b0e
sonhuynhpham <sonhuynhpham@kms-technology.com>
2019-09-11 10:39:17 +0700

    Add test suite

---
+++ Test Suites/JMeter Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>JMeter Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>1032f5b6-662b-421f-86ec-a42287ceca89</testSuiteGuid>
+   <testCaseLink>
+      <guid>0ab71f77-07ce-4764-a4db-cb094e8aefdd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JMeter</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
